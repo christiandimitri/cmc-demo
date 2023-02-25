6e7b2e0b79f623f4c06ccab831bca345a8a91af8
Christian Dimitri <christian.j.dimitrii@gmail.com>
2023-02-25 10:29:40 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <div class="rounded-lg bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
+      <div>
+        <span
+          class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg"
+        >
+          <svg
+            class="h-6 w-6 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <!-- ... -->
+          </svg>
+        </span>
+      </div>
+      <h3 class="mt-5 text-base font-medium tracking-tight text-slate-900 dark:text-white">
+        Writes Upside-Down
+      </h3>
+      <p class="mt-2 text-sm text-slate-500 dark:text-slate-400">
+        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even
+        works in outer space.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
